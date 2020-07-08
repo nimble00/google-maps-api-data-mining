e6c516d81e71ff99986d13d2d5859638e547485b
Rahul Nimbal <onlynone00@gmail.com>
2020-07-08 23:07:51 +0530

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -14,9 +14,9 @@ for i in range(100,1000):
 	for url in search(ss, tld='com', lang='es', stop=5): # search() will return top 5 urls
 		if str(i) not in url:
 			file.write(url + '\n')
-			# Note that it is possible that the 'x new cases' is missing from the
-			# url, but it is there in the webpage. So, use 'requests' to get the
-			# get the webpage, and 'beautifulsoup' to parse its text. Then, check
-			# if the 'x new cases' is there or not.
+		# Note that it is possible that the 'x new cases' is missing from the
+		# url, but it is there in the webpage. So, use 'requests' to get the
+		# get the webpage, and 'beautifulsoup' to parse its text. Then, check
+		# if the 'x new cases' is there or not.
 	print(i)
 file.close()
